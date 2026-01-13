52d71753a7e96f4072443149ffd29937b483e907
dongwlin <dongwlin@foxmail.com>
2026-01-13 17:56:39 +0800

    refactor: remove authentication and user store components, streamline routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,32 @@
-import Home from "@/views/Home";
+import Home from '@/views/Home'
 // import Login from "@/views/Login";
-import NotFound from "@/views/NotFound";
-import Order from "@/views/Order";
-import Settings from "@/views/Settings";
-import Statistic from "@/views/Statistics";
-import { createBrowserRouter } from "react-router";
-// import { AuthRouter, ProtectedRouter } from "./Routes";
+import NotFound from '@/views/NotFound'
+import Order from '@/views/Order'
+import Settings from '@/views/Settings'
+import Statistic from '@/views/Statistics'
+import { createBrowserRouter } from 'react-router'
 
 const router = createBrowserRouter([
-  // {
-  //   path: "/login",
-  //   element: <AuthRouter><Login /></AuthRouter>
-  // },
   {
-    path: "/",
-    // element: <ProtectedRouter><Home /></ProtectedRouter>
-    element: <Home />
+    path: '/',
+    element: <Home />,
   },
   {
-    path: "/order",
-    // element: <ProtectedRouter><Order /></ProtectedRouter>
-    element: <Order />
+    path: '/order',
+    element: <Order />,
   },
   {
-    path: "/statistics",
-    // element: <ProtectedRouter><Statistic /></ProtectedRouter>
-    element: <Statistic />
+    path: '/statistics',
+    element: <Statistic />,
   },
   {
-    path: "/settings",
-    // element: <ProtectedRouter><Settings /></ProtectedRouter>
-    element: <Settings />
+    path: '/settings',
+    element: <Settings />,
   },
   {
-    path: "*",
-    element: <NotFound />
-  }
+    path: '*',
+    element: <NotFound />,
+  },
 ])
 
 export default router
--- src/views/Settings/index.tsx
+++ src/views/Settings/index.tsx
@@ -1,15 +1,13 @@
-import { CarbonArrowLeft } from "@/components/Icon";
-import { useOrderStore } from "@/store/order";
-import { useUserStore } from "@/store/users";
-import React from "react";
-import { useNavigate } from "react-router";
+import { CarbonArrowLeft } from '@/components/Icon'
+import { useOrderStore } from '@/store/order'
+import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Settings: React.FC = () => {
   const navigate = useNavigate()
-  const {loginout} = useUserStore()
 
   const NavToHomeView = () => {
-    navigate("/", {
+    navigate('/', {
       replace: true,
     })
   }
@@ -19,15 +17,21 @@ const Settings: React.FC = () => {
   return (
     <div>
       <div className="fixed top-0 left-0 right-0 z-10 h-16 mt-4 bg-base-200 flex justify-between items-center">
-        <button className="btn btn-ghost" onClick={NavToHomeView}>
+        <button
+          className="btn btn-ghost"
+          onClick={NavToHomeView}
+        >
           <CarbonArrowLeft className="size-10" />
         </button>
       </div>
       <div className="mt-20 pt-2 flex flex-col items-center justify-center gap-y-6">
         <div>设置</div>
-        <button className="btn btn-error" onClick={clear}>清空</button>
-        <button className="px-4 py-2 bg-red-500 text-white rounded font-semibold hover:bg-red-600 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-         onClick={loginout}>退出登录</button>
+        <button
+          className="btn btn-error"
+          onClick={clear}
+        >
+          清空
+        </button>
       </div>
     </div>
   )
