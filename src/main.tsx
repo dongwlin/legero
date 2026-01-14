ae5e3febedc799377c153c22310722fd733abee4
dongwlin <dongwlin@foxmail.com>
2026-01-14 14:03:58 +0800

    style: update quotes and semicolons in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,18 @@ import './index.css'
 import './tailwind.css'
 import App from './App.tsx'
 import dayjs from 'dayjs'
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 import 'dayjs/locale/zh-cn'
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
+dayjs.extend(utc)
+dayjs.extend(timezone)
 dayjs.locale('zh-cn')
 
-dayjs.tz.setDefault("Asia/Shanghai"); 
+dayjs.tz.setDefault('Asia/Shanghai')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
