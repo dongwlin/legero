713355b9d62d7fbcc6bef3ac9d5aa0ffc29c7bf2
dongwlin <dongwlin@foxmail.com>
2026-01-21 18:48:34 +0800

    style: standardize quotes and formatting in DiningSelector component

--- src/views/Order/components/selectors/DiningSelector.tsx
+++ src/views/Order/components/selectors/DiningSelector.tsx
@@ -1,6 +1,10 @@
-import React from "react"
-import { DINING_METHODS, PACKAGING_OPTIONS, PACKAGING_METHODS } from "../constants"
-import { DiningMethod, Packaging, PackagingMethod } from "@/types"
+import React from 'react'
+import {
+  DINING_METHODS,
+  PACKAGING_OPTIONS,
+  PACKAGING_METHODS,
+} from '../constants'
+import { DiningMethod, Packaging, PackagingMethod } from '@/types'
 
 interface DiningSelectorProps {
   diningMethod: DiningMethod
@@ -22,19 +26,19 @@ export const DiningSelector: React.FC<DiningSelectorProps> = ({
   onDiningMethodChange,
   onPackagingChange,
   onPackagingMethodChange,
-  showTakeoutOptions
+  showTakeoutOptions,
 }) => {
   return (
     <>
-      <label className="fieldset-label text-xl">
-        <span className="mr-2">就餐方式</span>
+      <label className='fieldset-label text-xl'>
+        <span className='mr-2'>就餐方式</span>
         <select
-          name="就餐方式"
-          className="select text-xl"
+          name='就餐方式'
+          className='select text-xl'
           value={diningMethod}
           onChange={(e) => onDiningMethodChange(e.target.value as DiningMethod)}
         >
-          {DINING_METHODS.map(method => (
+          {DINING_METHODS.map((method) => (
             <option key={method}>{method}</option>
           ))}
         </select>
@@ -42,29 +46,33 @@ export const DiningSelector: React.FC<DiningSelectorProps> = ({
 
       {showTakeoutOptions && (
         <>
-          <label className="fieldset-label text-xl">
-            <span className="mr-2">打包包装</span>
+          <label className='fieldset-label text-xl'>
+            <span className='mr-2'>打包包装</span>
             <select
-              name="打包包装"
-              className="select text-xl"
+              name='打包包装'
+              className='select text-xl'
               value={packaging}
               onChange={(e) => onPackagingChange(e.target.value as Packaging)}
             >
-              {PACKAGING_OPTIONS.map(option => (
-                <option key={option} value={option}>{option}</option>
+              {PACKAGING_OPTIONS.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
               ))}
             </select>
           </label>
 
-          <label className="fieldset-label text-xl">
-            <span className="mr-2">打包方式</span>
+          <label className='fieldset-label text-xl'>
+            <span className='mr-2'>打包方式</span>
             <select
-              name="打包方式"
-              className="select text-xl"
+              name='打包方式'
+              className='select text-xl'
               value={packagingMethod}
-              onChange={(e) => onPackagingMethodChange(e.target.value as PackagingMethod)}
+              onChange={(e) =>
+                onPackagingMethodChange(e.target.value as PackagingMethod)
+              }
             >
-              {PACKAGING_METHODS.map(method => (
+              {PACKAGING_METHODS.map((method) => (
                 <option key={method}>{method}</option>
               ))}
             </select>
